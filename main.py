ef3b6fabbc1d20aae9743e5bb6fe0e821bd4bb12
70by666 <70bytriplesix666@gmail.com>
2023-04-05 15:35:12 +0300

    add_number, delkb, fixes

--- main.py
+++ main.py
@@ -1,38 +1,61 @@
 ﻿import os
 
 import openai
-
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from dotenv import find_dotenv, load_dotenv
 
-from dotenv import load_dotenv, find_dotenv
-
+from keyboards import kb
+from models import Numbers
+from numbers_model import new_number
 
 load_dotenv(find_dotenv())
 
-openai.api_key = os.getenv("CHATGPT_TOKEN")
-bot = Bot(os.getenv("BOT_TOKEN"))
+# config
+openai.api_key = str(os.getenv('CHATGPT_TOKEN'))
+bot = Bot(str(os.getenv('BOT_TOKEN')))
 dp = Dispatcher(bot)
 
 
-kb = [
-    [types.KeyboardButton(text="/start")],
-]
-keyboard = types.ReplyKeyboardMarkup(
-    keyboard=kb,
-    resize_keyboard=True,
-    input_field_placeholder="Че хочешь?!",
-)
+class FSMSearch(StatesGroup):
+    name = State()
 
 
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.answer("Пиши /chatgpt и что тебе нужно")  
+async def start(message: types.Message):
+    """
+    Список команд и кнопки
+    """
+    await bot.send_message(
+        message.chat.id, 
+        'Выполнять команды строго по форме ниже:\n'
+        '/chatgpt [text]\n'
+        '/addnumber [last_name] [first_name] [patronymic] [number]',
+        reply_markup=kb,
+    )  
+
+
+# numbers
+@dp.message_handler(commands=['addnumber'])
+async def add_number(message: types.Message):
+    """
+    Добавить новую запись в базу
+    """
+    result = new_number(message)
+    if result:
+        await bot.send_message(message.chat.id, 'Данные внесены')
+    else:
+        await bot.send_message(message.chat.id, 'Команда использована неверно')
     
     
 @dp.message_handler(commands=['chatgpt'])
-async def send(message: types.Message):
+async def chatgpt(message: types.Message):
+    """
+    API open ai chatgpt
+    """
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model='text-davinci-003',
         prompt=message.text,
         temperature=0.9,
         max_tokens=2000,
@@ -40,8 +63,25 @@ async def send(message: types.Message):
         frequency_penalty=0.0,
         presence_penalty=0.6,
     )
-    await message.answer(response['choices'][0]['text'])
-    
+    await bot.send_message(message.chat.id, response['choices'][0]['text'])
+
+
+@dp.message_handler(commands=['id'])
+async def send(message: types.Message):
+    """
+    Команда чтобы получить id пользователя и чата
+    """
+    await bot.send_message(
+        message.chat.id,
+        f'chat: {message.chat.id}, user: {message.from_user.id}',
+    )
+
+
+@dp.message_handler(commands=['убратькнопки'])
+async def delkb(message: types.Message):
+    mk = types.ReplyKeyboardRemove()
+    await bot.send_message(message.chat.id, 'ok', reply_markup=mk)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
